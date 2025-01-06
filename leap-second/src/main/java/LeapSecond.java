4e338e2d96700ee6eeea487418d6abf4ea23f4a6
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2025-01-06 08:06:38 +0100

    Convert main-code to junit5-tests

--- leap-second/src/main/java/LeapSecond.java
+++
@@ -1,7 +0,0 @@
-void main() {
-	Instant start = Instant.parse("2016-12-31T23:59:59Z");
-	Instant end = Instant.parse("2016-12-31T23:59:60Z");
-	System.out.println("Start: " + start);
-	System.out.println("End: " + end);
-	System.out.println("Duration: " + (end.getEpochSecond() - start.getEpochSecond()) + " seconds");
-}
---
+++ leap-second/src/test/java/LeapSecondTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @see Instant
+ */
+public class LeapSecondTest {
+
+	@Test
+	void java_parses_leap_second_to_second_59() {
+		assertEquals(
+				Instant.parse("2016-12-31T23:59:59Z"),
+				Instant.parse("2016-12-31T23:59:60Z")
+		);
+	}
+
+	@Test
+	void java_jumps_over_leap_second() {
+		assertEquals(
+				Instant.parse("2016-12-31T23:59:59Z").plusSeconds(1),
+				// you might have guessed Instant.parse("2016-12-31T23:59:60Z")
+				Instant.parse("2017-01-01T00:00:00Z")
+		);
+	}
+
+	@Test
+	void testDurationCalculation() {
+		assertEquals(1,
+				Instant.parse("2017-01-01T00:00:00Z").getEpochSecond() -
+						Instant.parse("2016-12-31T23:59:59Z").getEpochSecond()
+		);
+	}
+}
