b495d7c016004a44a6eaf07c522dc51e5293dba1
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2025-01-09 08:51:23 +0100

    Add calculations with ZonedDateTime

--- calculating-times/src/test/java/CalculationTest.java
+++ calculating-times/src/test/java/CalculationTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 import static java.util.Arrays.stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +23,22 @@ class CalculationTest {
 		assertEquals(
 				LocalDateTime.parse("2025-03-30T14:00:00"),
 				LocalDateTime.parse("2025-03-29T14:00:00").plusDays(1)
+		);
+	}
+
+	@Test
+	void zonedDateTime_simply_adds_days() {
+		assertEquals(
+				ZonedDateTime.parse("2025-03-30T14:00:00+02:00[Europe/Paris]"),
+				ZonedDateTime.parse("2025-03-29T14:00:00+01:00[Europe/Paris]").plusDays(1)
+		);
+	}
+
+	@Test
+	void zonedDateTime_simply_adds_s() {
+		assertEquals(
+				ZonedDateTime.parse("2025-03-30T15:00:00+02:00[Europe/Paris]"),
+				ZonedDateTime.parse("2025-03-29T14:00:00+01:00[Europe/Paris]").plusHours(24)
 		);
 	}
 }
