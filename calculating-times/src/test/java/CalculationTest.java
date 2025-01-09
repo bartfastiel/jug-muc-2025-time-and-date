47cca133bc3b81daf73cf89f5b8aede2ef644dfa
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2025-01-09 08:53:33 +0100

    Add calculations with OffsetDateTime

--- calculating-times/src/test/java/CalculationTest.java
+++ calculating-times/src/test/java/CalculationTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 
 import static java.util.Arrays.stream;
@@ -39,6 +40,22 @@ class CalculationTest {
 		assertEquals(
 				ZonedDateTime.parse("2025-03-30T15:00:00+02:00[Europe/Paris]"),
 				ZonedDateTime.parse("2025-03-29T14:00:00+01:00[Europe/Paris]").plusHours(24)
+		);
+	}
+
+	@Test
+	void offsetDateTime_simply_adds_days() {
+		assertEquals(
+				OffsetDateTime.parse("2025-03-30T14:00:00+01:00"),
+				OffsetDateTime.parse("2025-03-29T14:00:00+01:00").plusDays(1)
+		);
+	}
+
+	@Test
+	void offsetDateTime_simply_adds_s() {
+		assertEquals(
+				OffsetDateTime.parse("2025-03-30T14:00:00+01:00"),
+				OffsetDateTime.parse("2025-03-29T14:00:00+01:00").plusHours(24)
 		);
 	}
 }
