d67990cbc7c05e07c6bba17a24b9672a776c60bf
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2025-01-14 08:34:29 +0100

    Code of live presentation

--- code-example-01-baloon-pop/src/main/java/Example.java
+++ code-example-01-baloon-pop/src/main/java/Example.java
@@ -1,2 +1,16 @@
+import static java.time.temporal.ChronoUnit.MINUTES;
+
 void main() {
+
+	var balloonPopped = ZonedDateTime.of(2025, 1, 13, 19, 16, 45, 0, ZoneId.of("Europe/Berlin"));
+	System.out.println(balloonPopped);
+
+	var now = ZonedDateTime.now();
+	System.out.println(now);
+	var minutesDiff = MINUTES.between(balloonPopped, now);
+
+	System.out.println(minutesDiff);
+
+
+
 }
